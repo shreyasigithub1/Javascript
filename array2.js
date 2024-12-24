612a80e2a7a1f9371a1832852ba51449d5693be5
shreyasigithub1 <shreyasi.bhattacharya18@gmail.com>
2024-12-24 19:52:17 +0530

    Array 2 commit

---
+++ array2.js
@@ -0,0 +1,25 @@
+const student =[
+    "Tina",
+    "Raj",
+    "Bobby",
+    "Seema",
+    "Alex",
+    "Sita"
+]
+
+slength=student.length;
+//Using for loop
+/*for(let i=0;i<slength;i++){
+console.log( `Roll no- ${i+1} : ${student[i]}`);
+}*/
+
+//for -in loop
+for(let i in student){
+    console.log( `Roll no- ${Number
+        (i)+1} : ${student[i]}`);  
+}
+
+//for..of loop
+for(let students of student){
+    console.log(students);  
+}
